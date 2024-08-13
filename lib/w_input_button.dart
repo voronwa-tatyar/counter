a0e20c4fb813fdddc7f7b6e5ddb08693caf3154f
voronwa-tatyar <voronwa.tatyar@gmail.com>
2024-08-14 08:09:01 +0900

    Increase input button size

--- lib/w_input_button.dart
+++ lib/w_input_button.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class InputButton extends StatelessWidget {
   const InputButton({super.key, required this.callback, required this.number});
+
   final Function(int) callback;
   final int number;
 
@@ -9,7 +10,7 @@ class InputButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () => callback.call(number),
-      child: Text('$number'),
+      child: SizedBox(width: 60, height: 60, child: Center(child: Text('$number', style: const TextStyle(fontSize: 40),))),
     );
   }
 }
